058db75d611adda03ab60adfb35960d5b4206df7
Attiq <danilo.neves21@outlook.com>
2020-01-05 18:27:54 -0300

    alteracao em lancamento

--- algamoney-ui/src/app/lancamentos/lancamentos.module.ts
+++ algamoney-ui/src/app/lancamentos/lancamentos.module.ts
@@ -1,8 +1,16 @@
-import { LancamentoCadastroComponent } from './../lancamento-cadastro/lancamento-cadastro.component';
+import { InputMaskModule } from 'primeng/components/inputmask/inputmask';
+import { InputTextareaModule, CalendarModule, SelectButtonModule, DropdownModule } from 'primeng/primeng';
+import { TooltipModule } from 'primeng/components/tooltip/tooltip';
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/components/button/button';
+import { InputTextModule } from 'primeng/components/inputtext/inputtext';
+import { LancamentoCadastroComponent } from './lancamento-cadastro/lancamento-cadastro.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
 import { LancamentosGridComponent } from './lancamentos-grid/lancamentos-grid.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CurrencyMaskModule } from 'ng2-currency-mask';
 
 
 
@@ -13,7 +21,17 @@ import { LancamentosGridComponent } from './lancamentos-grid/lancamentos-grid.co
     LancamentosGridComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    InputTextModule,
+    ButtonModule,
+    TableModule,
+    TooltipModule,
+    InputTextareaModule,
+    CalendarModule,
+    SelectButtonModule,
+    DropdownModule,
+    CurrencyMaskModule
   ],
   exports: [
     LancamentoCadastroComponent,
